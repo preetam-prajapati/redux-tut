f7462f75031a35864560c9185549fca3f4f42b20
preetam-prajapati <preetamprajapati9165@gmail.com>
2022-03-29 04:35:39 +0530

    commit -m public

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+//
+import {createStore} from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './Service/reducers/index'
+const store=createStore(rootReducer)
+console.log('store data',store)
+//
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+ </Provider>,
   document.getElementById('root')
 );
 
