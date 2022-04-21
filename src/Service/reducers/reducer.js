78d152e90e642c0343cf418cdbb8416e4ba98f75
PawanMehar <75157244+PawanMehar@users.noreply.github.com>
2022-04-21 16:49:37 +0530

    initial commit

--- src/Service/reducers/reducer.js
+++ src/Service/reducers/reducer.js
@@ -1,26 +1,31 @@
-import { ADD_TO_CART, REMOVE_TO_CART } from "../constants"
+import { ADD_TO_CART, REMOVE_TO_CART } from "../constants";
 const initialState = {
-    cardData: []
-    
-}
-export default function cardItem(state = initialState,action) {
-    
-
-    switch (action.type) {
-        case ADD_TO_CART:
-            console.log('reducer',action)
-        return {
-                ...state,
-                cardData: action.data                  
+  counts: { Hamburger: 0, Fries: 0, Coke: 0, Pepsi: 0 }
+};
+export default function cardItem(state = initialState, action) {
+  switch (action.type) {
+    case ADD_TO_CART:
+      console.log("reducer", state);
+      let addName = action.data.name;
+      let addValue = state.counts[addName] + 1;
+      return {
+        ...state,
+        counts: {
+          ...state.counts,
+          [addName]: addValue
         }
-        case REMOVE_TO_CART:
-            return{
-                ...state,
-
-    }
-        
-            break;
-            default:
-                return state
-    }
+      };
+    case REMOVE_TO_CART:
+      let removeName = action.data.name;
+      let removeValue = state.counts[removeName] - 1;
+      return {
+        ...state,
+        counts: {
+          ...state.counts,
+          [removeName]: removeValue
+        }
+      };
+    default:
+      return state;
+  }
 }
---
+++ src/constants/constants.js
@@ -0,0 +1,26 @@
+export const cardData = [
+  {
+    title: "Hamburger",
+    price: 200,
+    src:
+      "https://media.istockphoto.com/photos/hamburger-isolated-on-white-background-picture-id666651868?k=20&m=666651868&s=612x612&w=0&h=a84opTEm9hyv59gfW5tuuXMAG7ZnZe54U57hKtLVaSs="
+  },
+  {
+    title: "Fries",
+    price: 100,
+    src:
+      "https://images.pexels.com/photos/1583884/pexels-photo-1583884.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  },
+  {
+    title: "Coke",
+    price: 50,
+    src:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKSJAINgfci_TctexqWGWAl7auKwwPsCD4_m_z8i_DdV74wFLys5Gb0zkUX2GFvxkDDG0&usqp=CAU"
+  },
+  {
+    title: "Pepsi",
+    price: 50,
+    src:
+      "https://www.pepsi.com/en-us/uploads/images/social-share.jpg?mtime=20180110134930"
+  }
+];
